1fc452d51e0cda4581ff8ac62bd2fed16beeab20
laaouini <wassel.laouini@esprit.tn>
2022-02-23 14:22:46 +0100

    terratest1

--- IaaC/outputs.tf
+++ IaaC/outputs.tf
@@ -3,9 +3,9 @@ output "resource_group_name" {
   value = azurerm_resource_group.iotStorage.name
 }
 output "storage_account_name" {
-  value = azurerm_storage_account.storagewas.name
+  value = azurerm_storage_account.storagewass.name
 }
 
 output "storage_container_name" {
-  value = azurerm_storage_container.edge.name
+  value = azurerm_storage_container.edgee.name
 }
